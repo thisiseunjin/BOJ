5e41448ea9b9f511601a8ccc3c947463a7511114
thisiseunjin <98890934+thisiseunjin@users.noreply.github.com>
2025-01-19 23:02:25 +0900

    [Silver II] Title: N과 M (11), Time: 1228 ms, Memory: 255460 KB -BaekjoonHub

---
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.java
@@ -0,0 +1,65 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    static int N;
+    static int M;
+    static StringBuilder sb;
+    static int[] nums;
+    static int[] selected;
+    static Set<String> set;
+    
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        sb = new StringBuilder();
+        set = new LinkedHashSet<>();
+
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        nums = new int[N];
+        selected = new int[M];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i=0;i<N;i++){
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(nums);
+
+
+        comb(0,0);
+
+        for(String str:set){
+            st = new StringTokenizer(str);
+            for(int i=0;i<M;i++){
+                sb.append(Integer.parseInt(st.nextToken())).append(" ");
+            }
+            sb.append("\n");
+        }
+      
+        System.out.println(sb);
+    }
+
+    public static void comb(int cnt, int start){
+        if(cnt==M){
+            StringBuilder res = new StringBuilder();
+            for(int i=0;i<M;i++){
+                res.append(selected[i]).append(" ");
+            }
+
+            set.add(res.toString());
+            return;
+        }
+
+        for(int i=0;i<N;i++){
+            selected[cnt] = nums[i];
+            comb(cnt+1, i+1);
+        }
+    }
+}
