e6141b04258ed960e7969876b94290fb2d3613cf
thisiseunjin <98890934+thisiseunjin@users.noreply.github.com>
2025-01-19 23:19:21 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 100 ms, Memory: 14336 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+class Main {
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+        System.out.println(input.length());
+        
+    }
+}
