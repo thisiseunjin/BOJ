5f8937cadd4c76308db76003e57370ef9f565e49
thisiseunjin <98890934+thisiseunjin@users.noreply.github.com>
2025-01-28 23:42:09 +0900

    [Bronze V] Title: A-B, Time: 100 ms, Memory: 14212 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        System.out.println(a-b);
+    }
+}
