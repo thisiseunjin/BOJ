8ca646dd9666dd71eddf89a258d9316627cd63ad
silverstar_isjin <98890934+thisiseunjin@users.noreply.github.com>
2025-02-19 17:10:40 +0900

    [Silver V] Title: 배열 합치기, Time: 1416 ms, Memory: 343164 KB -BaekjoonHub

--- 백준/Silver/11728. 배열 합치기/배열 합치기.java
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.java
@@ -1,14 +1,14 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
     static int N;
     static int M;
     static StringBuilder sb = new StringBuilder();
-    static PriorityQueue<Integer> q = new PriorityQueue<>();
+    //    static PriorityQueue<Integer> q = new PriorityQueue<>();
+    static ArrayList<Integer> list = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -20,15 +20,16 @@ public class Main {
 
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            q.add(Integer.parseInt(st.nextToken()));
+            list.add(Integer.parseInt(st.nextToken()));
         }
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < M; i++) {
-            q.add(Integer.parseInt(st.nextToken()));
+            list.add(Integer.parseInt(st.nextToken()));
         }
 
-        while (!q.isEmpty()){
-            sb.append(q.poll()).append(" ");
+        Collections.sort(list);
+        for(int n:list){
+            sb.append(n).append(" ");
         }
 
         System.out.println(sb);
