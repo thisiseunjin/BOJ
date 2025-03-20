f14cb5dd327f7c05c7c32d5a35b740a18bd6e0c3
silverstar_isjin <98890934+thisiseunjin@users.noreply.github.com>
2025-03-20 21:27:34 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 2544 ms, Memory: 322504 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -1,22 +1,22 @@
-import java.util.PriorityQueue;
-import java.util.Scanner;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 
 public class Main {
     static int N;
+    static int[] nums;
     static PriorityQueue<Integer> q = new PriorityQueue<>();
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         N = sc.nextInt();
+        nums = new int[N];
         for (int i = 0; i < N; i++) {
-            q.add(sc.nextInt());
+            nums[i] = sc.nextInt();
         }
 
         StringBuilder sb = new StringBuilder();
-        while (!q.isEmpty()){
-            sb.append(q.poll()).append("\n");
+        Arrays.sort(nums);
+        for(int n:nums){
+            sb.append(n).append("\n");
         }
         System.out.print(sb);
     }
