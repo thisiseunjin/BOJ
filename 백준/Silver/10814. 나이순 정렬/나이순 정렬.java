298c20648bf68b9b6176d15e19dcd422b7397e54
silverstar_isjin <98890934+thisiseunjin@users.noreply.github.com>
2025-05-10 19:02:59 +0900

    [Silver V] Title: 나이순 정렬, Time: 1204 ms, Memory: 56092 KB -BaekjoonHub

---
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -0,0 +1,50 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        Person[] people = new Person[N];
+        
+        for(int i=0;i<N;i++){
+            st = new StringTokenizer(br.readLine());
+           
+            int age = Integer.parseInt(st.nextToken());
+            String name = st.nextToken();
+
+            people[i] = new Person(age, name,i);
+        }
+
+        Arrays.sort(people);
+
+        for(Person p : people){
+            System.out.println(p);
+        }
+    }
+
+    public static class Person implements Comparable<Person>{
+        String name;
+        int age;
+        int order;
+
+        public Person(int age, String name, int order){
+            this.name = name;
+            this.age = age;
+        }
+
+        public String toString(){
+            return this.age+" "+this.name;
+        }
+
+
+        public int compareTo(Person p){
+            if(this.age == p.age){
+                return this.order-p.order;
+            }
+            return this.age - p.age;
+        }
+    }
+}
