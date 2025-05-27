8fc51794bc4ce0e8fb9a67f6782dde37662b0089
silverstar_isjin <98890934+thisiseunjin@users.noreply.github.com>
2025-05-27 20:17:26 +0900

    [Silver I] Title: 쿼드트리, Time: 116 ms, Memory: 14112 KB -BaekjoonHub

--- 백준/Silver/1992. 쿼드트리/쿼드트리.java
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.java
@@ -19,29 +19,28 @@ public class Main {
                 map[i][j] = input.charAt(j) - '0';
             }
         }
-
-        System.out.println(compression(N, 0, 0));
+        compression(N, 0, 0);
+        System.out.println(sb);
 
     }
 
-    public static String compression(int size, int startX, int startY) {
+    public static void compression(int size, int startX, int startY) {
         //지금 압축 가능한가?
         int compressed = isPossible(size, startX, startY);
 
         if (compressed != -1) {
-            return compressed + "";
+            sb.append(compressed);
+            return;
         }
 
-
+        sb.append("(");
         //4등분
-        String s1 = compression(size / 2, startX, startY);
-        String s2 = compression(size / 2, startX, startY+ (size / 2));
-        String s3 = compression(size / 2, startX+ (size / 2), startY );
-        String s4 = compression(size / 2, startX + (size / 2), startY + (size / 2));
+        compression(size / 2, startX, startY);
+        compression(size / 2, startX, startY + (size / 2));
+        compression(size / 2, startX + (size / 2), startY);
+        compression(size / 2, startX + (size / 2), startY + (size / 2));
 
-        if (s1.equals("0") && s2.equals("0") && s3.equals("0") && s4.equals("0")) return "0";
-        if (s1.equals("1") && s2.equals("1") && s3.equals("1") && s4.equals("1")) return "1";
-        else return "(" + s1 + s2 + s3 + s4 + ")";
+        sb.append(")");
     }
 
     public static int isPossible(int size, int startX, int startY) {
