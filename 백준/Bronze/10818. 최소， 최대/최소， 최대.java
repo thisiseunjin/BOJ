161a018b05e6ce80cc5434b65c10d99d6473e89c
thisiseunjin <98890934+thisiseunjin@users.noreply.github.com>
2025-12-18 15:42:24 +0900

    [Bronze III] Title: 최소, 최대, Time: 1184 ms, Memory: 108160 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i=0;i<N;i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+        System.out.printf("%d %d", arr[0], arr[N-1]);
+    }
+}
