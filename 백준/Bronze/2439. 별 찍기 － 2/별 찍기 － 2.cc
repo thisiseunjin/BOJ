227d1310ef3bc2079ef7c74c85ab651f27f9b28c
thisiseunjin <98890934+thisiseunjin@users.noreply.github.com>
2025-12-18 15:50:52 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main() {
+    int n=0;
+    scanf("%d", &n);
+    for(int i=0;i<n;i++){
+         for(int j=0;j<n-i-1;j++){
+             printf(" ");
+         }
+        for(int j=n-i-1;j<n;j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+}
