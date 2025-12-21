cddd0f46c5413eb127da9c049cd9962b08d2a284
thisiseunjin <98890934+thisiseunjin@users.noreply.github.com>
2025-12-21 20:01:59 +0900

    [level 1] Title: 음양 더하기, Time: 0.07 ms, Memory: 79.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int sum=0;
+        for(int i=0;i<absolutes.length;i++){
+            if(!signs[i]){
+                absolutes[i]*=-1;
+            }
+            sum+=absolutes[i];
+        }
+        
+        return sum;
+    }
+}
