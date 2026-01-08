864c52fb68db9458d534230f8b84a294e6113505
thisiseunjin <98890934+thisiseunjin@users.noreply.github.com>
2026-01-08 22:50:48 +0900

    [Silver V] Title: 올림픽, Time: 108 ms, Memory: 14192 KB, Score: 8 point -BaekjoonHub

---
+++ 백준/Silver/8979. 올림픽/올림픽.java
@@ -0,0 +1,68 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    static int N;
+    static int K;
+    static ArrayList<int[]> list = new ArrayList<>();
+    
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+        for(int i=0;i<4;i++){
+            st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int gold = Integer.parseInt(st.nextToken());
+            int silver = Integer.parseInt(st.nextToken());
+            int bronze = Integer.parseInt(st.nextToken());
+
+            list.add(new int[]{n,gold, silver, bronze});
+
+
+        }
+
+            Collections.sort(list, new Comparator<int[]>(){
+               public int compare(int[] n1, int n2[]){
+                   if(n1[1]!=n2[1]) return n2[1]-n1[1];
+                   if(n1[2]!=n2[2]) return n2[2]-n1[2];
+                   if(n1[3]!=n2[3]) return n2[3]-n1[3];
+                   return n1[0]-n2[0];
+               } 
+            });
+
+        int rank = 1;
+        int sum=1;
+        
+        for(int i=1;i<list.size();i++){
+            int[] tmp = list.get(i);
+            int num = tmp[0];
+            int gold = tmp[1];
+            int silver = tmp[2];
+            int bronze = tmp[3];
+
+          
+
+            int[] pre = list.get(i-1);
+            if(gold==pre[1] && silver == pre[2] && bronze==pre[3]){
+                sum++;
+            }else{
+                rank += sum;
+                sum=0;
+            }
+
+              if(num==K){
+                break;
+            }
+        }
+
+        System.out.println(rank);
+        
+    }
+}
